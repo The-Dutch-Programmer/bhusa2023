35c8100b1b8d8f08f4084d7802b1dcc2ff05531f
Jay Beale <jay.beale@gmail.com>
2023-08-05 15:34:39 -0700

    typo

--- bin/sync-exercises.sh
+++ bin/sync-exercises.sh
@@ -4,8 +4,8 @@ if [ ! -e ~/bh2023 ] ; then
   cd ~
   git clone https://github.com/bustakube/bhusa2023.git
 else
-  cd ~/bh2023
+  cd ~/bhusa2023
   git pull
 fi
 
-cp ~/bh2023/exercises/*.md /root/exercises/exercises/
+cp ~/bhusa2023/exercises/*.md /root/exercises/exercises/
